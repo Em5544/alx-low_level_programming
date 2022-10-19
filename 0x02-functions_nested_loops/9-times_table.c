0028cbd300805e4b0e70d1e35e9ba2d48f5e2334
Emmanuel <nwogwugwuemmanuel2@gmail.com>
2022-10-18 18:23:09 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,38 @@ void times_table(void)
 		{
 			op = a * b;
 			_putchar(44);
-																										_putchar(32);
-																										if (op <= 9)
-																										{
-																										_putchar(32);
-																										_putchar(op + 48);
-																										}
-																										else
-																										{
-																										_putchar((op / 10) + 48);
-																										_putchar((op % 10) + 48);
-																																															}
-																								}
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
 		_putchar('\n');
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
