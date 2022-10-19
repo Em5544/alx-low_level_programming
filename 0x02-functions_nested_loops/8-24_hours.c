18db6dd02c48e6fe48f83d401c04052a73c56370
Emmanuel <nwogwugwuemmanuel2@gmail.com>
2022-10-18 17:33:18 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,20 +16,19 @@ void jack_bauer(void)
 		{
 			for (k = 0; k <= 5; k++)
 			{
-																										for (m = 0; m <= 9; m++)
-																										{
-																											if (i == 2 && j > 3)
-																											{break;
-																																																	}
-																											_putchar(i + '0');
-																																																		_putchar(j + '0');
-																																																		_putchar(':');
-																																																		_putchar(k + '0');
-																																																		_putchar(m + '0');
-																																																		_putchar('\n');
-																																																																								}
-																										}
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
 		}
 	}
 }
-
